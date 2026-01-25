271e596d560621410d7d329b4eda5d338a2acc20
Omar Taha <otaha2138@gmail.com>
2026-01-24 22:54:24 -0600

    woah

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
     path('api/hello/', views.api_hello, name='api_hello'),
 ]
