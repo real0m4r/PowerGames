7c5fa31335f16eaa56d7a8cf260a7a1953cfa324
Omar Taha <otaha2138@gmail.com>
2026-01-25 14:20:48 -0600

    Added Neon Database

--- core/views.py
+++ core/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    context = {
-        'title': 'Welcome to PowerGames',
-        'message': 'Your Django application is running successfully!'
-    }
-    return render(request, 'core/home.html', context)
+    return render(request, 'core/home.html' )
