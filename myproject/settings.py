2c6296de64dffd07d45c7890c577aad211ed5e99
Omar Taha <otaha2138@gmail.com>
2026-01-25 14:21:48 -0600

    Updated settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -62,9 +62,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
     )
 }
 
