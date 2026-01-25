9e05349819588baed3cb3176de07706df0fd5380
Omar Taha <otaha2138@gmail.com>
2026-01-24 21:07:50 -0600

    Update WSGI config to match PythonAnywhere auto-generated template with correct paths

--- PowerGame/pythonanywhere_wsgi.py
+++ PowerGame/pythonanywhere_wsgi.py
@@ -1,17 +1,21 @@
-# This file is used by PythonAnywhere to run your Django application
-# Place this file at: /var/www/yourusername_pythonanywhere_com_wsgi.py on PythonAnywhere
+# This file contains the WSGI configuration required to serve up your
+# web application at http://omaressam.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Django project
 
 import os
 import sys
-from pathlib import Path
 
-# Add your project to the path
-project_home = '/home/yourusername/PowerGame'
+# add your project directory to the sys.path
+project_home = '/home/omaressam/PowerGames/PowerGame'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# Set up Django
+# set environment variable to tell django where your settings.py is
 os.environ['DJANGO_SETTINGS_MODULE'] = 'PowerGame.settings'
 
+# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
