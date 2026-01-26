84e9120c809da7f58ffb469ed0357a0c7bb575a6
Omar Taha <otaha2138@gmail.com>
2026-01-25 20:45:02 -0600

    navbar2 update

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from django.core.paginator import Paginator
 
 def home(request):
     games = Game.objects.all()
-    paginator = Paginator(games, 10)
+    paginator = Paginator(games, 9)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'core/home.html', {'games': games, 'page_obj': page_obj})
